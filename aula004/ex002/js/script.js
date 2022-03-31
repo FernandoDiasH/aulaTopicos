1a0135c7ddd8fb7a26921fdda53d3938e2fb8f5d
FernandoDiasH <100883137+FernandoDiasH@users.noreply.github.com>
2022-03-30 21:10:14 -0300

    Add files via upload

---
+++ aula004/ex002/js/script.js
@@ -0,0 +1,32 @@
+
+function contar10(){
+	for(let i = 1; i<11 ; i++){
+		let para = document.createElement('p');
+		para.textContent = i;
+		document.body.appendChild(para);
+		
+	}
+}
+
+function debtec(){
+	let para = document.createElement('p');
+	para.textContent = "Resumidamente, o technical debt (débito técnico, em tradução livre) acontece todas as vezes que um código de baixa qualidade ou com erros é lançado para atender apenas às demandas do momento. Apesar de agilizar o processo de lançamento do software, por exemplo, os problemas começam a surgir no futuro"
+	document.body.appendChild(para);
+}
+	
+
+function sonar(){
+	let para = document.createElement('p');
+	para.textContent = i;
+	document.body.appendChild(para);
+
+}
+		
+function jenkins(){
+	let para = document.createElement('p');
+	para.textContent = i;
+	document.body.appendChild(para);
+
+}
+
+
